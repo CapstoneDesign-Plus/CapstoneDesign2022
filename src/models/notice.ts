389a2ac7446c3ea9dcb6e1040ceca7a65e8908f1
byungjin <byungjin.dev@gmail.com>
2022-08-04 22:26:03 +0900

    #13 add Notice DTO

---
+++ src/models/notice.ts
@@ -0,0 +1,103 @@
+
+import { Schema, Model, Document, model } from "mongoose";
+
+// DTO
+export interface NoticeDTO {
+  readonly identifier: number,
+  readonly title: string,
+  readonly writer: string,
+  readonly content: string,
+  readonly header?: string,
+  readonly postedAt?: Date,
+  readonly revisedAt?: Date,
+}
+
+export interface INotice extends Document {
+  identifier: number,
+  title: string,
+  writer: string,
+  content: string,
+  header: string,
+  postedAt: Date,
+  revisedAt: Date,
+}
+
+export interface NoticeModel {
+  create(user : NoticeDTO) : Promise<INotice>,
+  findOne(user : NoticeDTO) : Promise<INotice>,
+}
+
+interface INoticeModel extends Model<INotice> {
+  findByWriter: (writer: string) => Promise<INotice[]>;
+  findByTitle: (title: string) => Promise<INotice[]>;
+  findByHeader: (header: string) => Promise<INotice[]>;
+  findByDate: (date : Date) => Promise<INotice[]>;
+  getAll: () => Promise<INotice[]>;
+}
+
+const NoticeSchema : Schema<INotice> = new Schema({
+  identifier: {
+    type: Number,
+    required : true,
+    unique: true,
+  },
+  title: {
+    type: String,
+    require: true,
+  },
+  writer: {
+    type: String,
+    require: true,
+  },
+  content: {
+    type: String,
+    require: true,
+  },
+  header: {
+    type: String,
+    require: false,
+  },
+  postedAt: {
+    type: Date,
+    default: Date.now
+  },
+  revisedAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+NoticeSchema.statics.findByWriter = function(writer: string) {
+  return this.find({
+    writer: {$regex: writer}
+  });
+}
+
+NoticeSchema.statics.findByTitle = function(title: string) {
+  return this.find({
+    title: {$regex: title}
+  });
+}
+
+NoticeSchema.statics.findByHeader = function(header: string) {
+  return this.find({
+    header,
+  });
+}
+
+/**
+ * 추후 추가
+ * @param date 
+ * @returns 
+ */
+NoticeSchema.statics.findByDate = function(date : Date) {
+  return this.find({})
+}
+
+NoticeSchema.statics.getAll = function() {
+  return this.find({});
+}
+
+const Notice = model<INotice, INoticeModel>("notice", NoticeSchema);
+
+export default Notice
