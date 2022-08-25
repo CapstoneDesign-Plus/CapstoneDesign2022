2880dd2401c5ead04375c20bb15a2b46eb48a09e
byungjin <byungjin.dev@gmail.com>
2022-08-25 18:02:53 +0900

    #28 board view

--- public/admin/assets/js/view.js
+++ public/admin/assets/js/view.js
@@ -1,32 +1,48 @@
 
+export default function getView(parent = document) {
+  return function(selectorQuery){
+    return parseView(
+      parent.querySelector(selectorQuery),
+      parent
+    );
+  }
+}
+
 /**
+ * 
+ * @param {Element} el 
  * @param {Element} parent 
  * @returns 
  */
-export default function getView(parent = document) {
-  /**
-   * @param {string} selector
-   */
-  return function(selector) {
+function parseView(el, parent = document){
+  if(!el) throw Error('element is Null');
+
+  return {
+    source: el,
+    select: getView(el),
     /**
-     * @type {Element}
+     * @param {string} text 
      */
-    const view = parent.querySelector(selector);
+    setText(text) {
+      this.source.innerHTML = text;
+    },
+    registerEvent(eventName, callback){
+      this.source.addEventListener(eventName, callback)
+    },
+    getValue() {
+      return this.source.value;
+    },
+    /**
+     * 
+     * @param {Element} element 
+     */
+    createAndAppendView(element) {
+      this.source.appendChild(element);
+      return parseView(element, this.source);
+    },
 
-    if(!view) throw Error('no view');
-
-    return {
-      source: view,
-      select: getView(view),
-      /**
-       * @param {string} text 
-       */
-      setText(text) {
-        this.source.innerHTML = text;
-      },
-      registerEvent(eventName, callback){
-        this.source.addEventListener(eventName, callback)
-      }
+    setAttr(key, value) {
+      this.source.setAttribute(key, value);
     }
   }
 }
