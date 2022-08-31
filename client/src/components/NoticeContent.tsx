d74110b9faf6c18ccf4e06d3819e5caed1e2b99f
byungjin <byungjin.dev@gmail.com>
2022-08-31 13:54:01 +0900

    fixed notice content error

--- client/src/components/NoticeContent.tsx
+++ client/src/components/NoticeContent.tsx
@@ -1,39 +1,30 @@
 
-import React from 'react';
-import { PromiseFn, useAsync } from 'react-async'
+import React, { useEffect, useState } from 'react';
+import { PromiseFn } from 'react-async'
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 interface NoticeData {
   content: string,
 }
 
-const loadNotice : PromiseFn<NoticeData> = async ({noticeId}) => {
-  const res = await fetch(`/api/v1/notice/get/${noticeId}`);
-
-  if(res.status != 200) throw Error('error');
-
-  return await res.json() as NoticeData;
-}
-
-
 export default function NoticeContent() {
 
   const { noticeId } = useParams();
-  
-  const { data: notice, error, isLoading } = useAsync<NoticeData>({
-    promiseFn: loadNotice,
-    noticeId,
-  });
-  
-  if (isLoading) return <div>Loading...</div>
-  if (error) return <div>Something went wrong: {error.message}</div>
-  if (notice)
-    return (
-      <div>
-        {notice.content}
-      </div>
-    )
-  return <div>
 
+  const [content, setContent] = useState('');
+
+  useEffect(()=>{
+    (async () => {
+      const res = await axios.get(`/api/v1/notice/get/${noticeId}`);
+
+      if(res.status != 200) throw Error('error');
+
+      setContent(res.data.content);
+
+    })().catch();
+  })
+  return <div>
+    {content}
   </div>
 }
