4c8f244b584ce9bc7c8925a15e073ef7d4c0a506
narinn-star <skfls2618@naver.com>
2022-10-23 20:09:10 +0900

    #46 MyPage_Complete

--- client/src/components/MyPage/Mypage.jsx
+++ client/src/components/MyPage/Mypage.jsx
@@ -54,7 +54,7 @@ const MypageStyle = styled.div`
     color: #49663c;
   }
   .btn-group {
-    margin:auto 5px;
+    margin: auto 5px;
     display: flex;
     justify-content: center;
   }
@@ -73,114 +73,119 @@ const MypageStyle = styled.div`
 
 function Mypage() {
   return (
-      <MypageStyle>
-        <Box
-          className="title"
-          sx={{ display: "flex", alignItems: "flex-end", mt: 3, ml: 2 }}
-        >
-          마이페이지
-        </Box>
-        <Box sx={{ display: "flex", alignItems: "flex-end", mt: 5 }}>
-          <Grid container spacing={2} sx={{margin:0}}>
-            {/* 이름, 이메일 */}
-            <Grid className="name" item xs={12} sm={12}>
-              홍길동
-            </Grid>
-            <Grid className="email" item xs={12} sm={12}>
-              Hong Gil Dong@gmail.com
-            </Grid>
-            {/* 재화 박스 */}
-            <Box
-              className="coin_box"
-              sx={{ display: "flex", alignItems: "flex-start",margin: 'auto', mt: 3, ml: 1}}
-            >
-              <Grid container spacing={2} style={{margin:0}}>
-                <Grid className="coin_title" item xs={12} sm={12}>
-                  잔여재화
-                </Grid>
-                <Grid className="coin_content" item xs={12} sm={2}>
-                  <Box sx={{ display: "flex", alignItems: "flex-start" }}>
-                    <img className="coin" alt="coin" src="\images\coin.png" />
-                    &nbsp;10,000
-                  </Box>
-                </Grid>
-                <Grid
-                  item
-                  xs={12}
-                  sm={8}
-                  sx={{ display: "flex", justifyContent: "flex-end", ml:5 }}
-                >
-                  <Chip
-                    className="coin_btn"
-                    label="충 전"
-                    clickable
-                    component="a"
-                    href="#"
-                    color="primary"
-                  />
-                </Grid>
+    <MypageStyle>
+      <Box
+        className="title"
+        sx={{ display: "flex", alignItems: "flex-end", mt: 3, ml: 2 }}
+      >
+        마이페이지
+      </Box>
+      <Box sx={{ display: "flex", alignItems: "flex-end", mt: 5 }}>
+        <Grid container spacing={2} sx={{ margin: 0 }}>
+          {/* 이름, 이메일 */}
+          <Grid className="name" item xs={12} sm={12}>
+            홍길동
+          </Grid>
+          <Grid className="email" item xs={12} sm={12}>
+            Hong Gil Dong@gmail.com
+          </Grid>
+          {/* 재화 박스 */}
+          <Box
+            className="coin_box"
+            sx={{
+              display: "flex",
+              alignItems: "flex-start",
+              margin: "auto",
+              mt: 3,
+              ml: 1,
+            }}
+          >
+            <Grid container spacing={2} style={{ margin: 0 }}>
+              <Grid className="coin_title" item xs={12}>
+                잔여재화
               </Grid>
-            </Box>
-            {/* 버튼 박스 */}
-            <Box>
-              <ButtonGroup className="btn-group" sx={{ display: "flex", alignItems: "flex-start"}} style={{marginTop: '50px'}}>
-                <Link to="/ChangeNickname">
-                  <Button
-                    className="btn"
-                    variant="contained"
-                    color="primary"
-                    sx={{
-                      borderTopLeftRadius: 20,
-                      borderBottomLeftRadius: 0,
-                    }}
-                  >
-                    닉네임 변경
-                  </Button>
-                </Link>
-                <Link to="/ChangePassword">
-                  <Button
-                    className="btn"
-                    variant="contained"
-                    color="primary"
-                    sx={{
-                      borderTopRightRadius: 20,
-                      borderBottomRightRadius: 0,
-                    }}
-                  >
-                    비밀번호 변경
-                  </Button>
-                </Link>
-              </ButtonGroup>
-              <ButtonGroup className="btn-group" sx={{ mt: 0 }}>
-                <Link to="/BuyList">
-                  <Button
-                    className="btn"
-                    variant="contained"
-                    color="primary"
-                    sx={{
-                      borderTopLeftRadius: 0,
-                      borderBottomLeftRadius: 20,
-                    }}
-                  >
-                    구매 내역
-                  </Button>
-                </Link>
+              <Grid className="coin_content" item xs={8}>
+                <Box sx={{ display: "flex", alignItems: "left" }}>
+                  <img className="coin" alt="coin" src="\images\coin.png" />
+                  &nbsp;10,000
+                </Box>
+              </Grid>
+              <Grid item xs={4}>
+              <Chip
+                      className="coin_btn"
+                      label="충 전"
+                      clickable
+                      component="a"
+                      href="#"
+                      color="primary"
+                    />
+              </Grid>
+            </Grid>
+          </Box>
+          {/* 버튼 박스 */}
+          <Box>
+            <ButtonGroup
+              className="btn-group"
+              sx={{ display: "flex", alignItems: "flex-start" }}
+              style={{ marginTop: "50px" }}
+            >
+              <Link to="/ChangeNickname">
                 <Button
                   className="btn"
                   variant="contained"
                   color="primary"
                   sx={{
-                    borderTopRightRadius: 0,
-                    borderBottomRightRadius: 20,
+                    borderTopLeftRadius: 20,
+                    borderBottomLeftRadius: 0,
                   }}
                 >
-                  로그아웃
+                  닉네임 변경
                 </Button>
-              </ButtonGroup>
-            </Box>
-          </Grid>
-        </Box>
-      </MypageStyle>
+              </Link>
+              <Link to="/ChangePassword">
+                <Button
+                  className="btn"
+                  variant="contained"
+                  color="primary"
+                  sx={{
+                    borderTopRightRadius: 20,
+                    borderBottomRightRadius: 0,
+                  }}
+                >
+                  비밀번호 변경
+                </Button>
+              </Link>
+            </ButtonGroup>
+            <ButtonGroup className="btn-group" sx={{ mt: 0 }}>
+              <Link to="/BuyList">
+                <Button
+                  className="btn"
+                  variant="contained"
+                  color="primary"
+                  sx={{
+                    borderTopLeftRadius: 0,
+                    borderBottomLeftRadius: 20,
+                  }}
+                >
+                  구매 내역
+                </Button>
+              </Link>
+              <Button
+                className="btn"
+                variant="contained"
+                color="primary"
+                sx={{
+                  borderTopRightRadius: 0,
+                  borderBottomRightRadius: 20,
+                }}
+              >
+                로그아웃
+              </Button>
+            </ButtonGroup>
+          </Box>
+        </Grid>
+      </Box>
+    </MypageStyle>
   );
 }
 
