26145c73bf7eff31e0cedd68a6fe687b1f375e06
narinn-star <skfls2618@naver.com>
2022-11-01 23:24:25 +0900

    (#50) Signin

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 import React, { useState } from "react";
 import { Box, Grid, Button } from "@mui/material";
-import {Link} from "react-router-dom"
-import axios from '../lib/axios';
+import { Link } from "react-router-dom";
+import axios from "../lib/axios";
 
 const SigninStyle = styled.div`
   top: 0;
@@ -54,34 +54,34 @@ const SigninStyle = styled.div`
   .link {
     text-align: center;
   }
-  .btn{
+  .btn {
     font-weight: bolder;
     font-size: 17px;
   }
 `;
 
-async function login (email, password) {
-  const response = await axios.post('v1/user/auth/login', { email, password});
+async function login(email, password) {
+  const response = await axios.post("v1/user/auth/login", { email, password });
   return response;
 }
 
 function SignIn() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleEmail = (e) => {
     setEmail(e.target.value);
-  }
+  };
 
   const handlePw = (e) => {
     setPassword(e.target.value);
-  }
+  };
 
   const handleClick = () => {
-    login(email, password).then(()=>{
-      console.log('Complete!')
+    login(email, password).then(() => {
+      console.log("Complete!");
     });
-  }
+  };
 
   return (
     <SigninStyle>
@@ -100,14 +100,27 @@ function SignIn() {
             Email
           </Grid>
           <Grid item xs={12} sx={{ ml: -1 }}>
-            <input className="input_email" autoFocus placeholder=" 이메일" type="email" value={email} onChange={handleEmail}/>
+            <input
+              className="input_email"
+              autoFocus
+              placeholder=" 이메일"
+              type="email"
+              value={email}
+              onChange={handleEmail}
+            />
           </Grid>
           {/* 비밀번호 */}
           <Grid className="input_title" item xs={12} sx={{ mt: 3 }}>
             Password
           </Grid>
           <Grid item xs={12} sx={{ ml: -1 }}>
-            <input className="input_pw" placeholder=" 비밀번호" type="password" value={password} onChange={handlePw}/>
+            <input
+              className="input_pw"
+              placeholder=" 비밀번호"
+              type="password"
+              value={password}
+              onChange={handlePw}
+            />
           </Grid>
         </Grid>
       </Box>
@@ -122,22 +135,27 @@ function SignIn() {
       >
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            <Button className="login_btn" variant="contained" color="primary" onClick={handleClick}>
+            <Button
+              className="login_btn"
+              variant="contained"
+              color="primary"
+              onClick={handleClick}
+            >
               로그인
             </Button>
           </Grid>
           <Grid className="link" item xs={6}>
-          <Link to="/FindPassword">
-            <Button className="btn" color="success">
-              비밀번호 재설정
-            </Button>
+            <Link to="/FindPassword">
+              <Button className="btn" color="success">
+                비밀번호 재설정
+              </Button>
             </Link>
           </Grid>
           <Grid className="link" item xs={6}>
             <Link to="/SignUp">
-            <Button className="btn" color="success">
-              회원 가입
-            </Button>
+              <Button className="btn" color="success">
+                회원 가입
+              </Button>
             </Link>
           </Grid>
         </Grid>
