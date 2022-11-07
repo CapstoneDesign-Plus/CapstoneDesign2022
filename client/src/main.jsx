466b96f6aca98ad71a73c397bc83f53c46a7932d
byungjin <byungjin.dev@gmail.com>
2022-11-07 19:05:03 +0900

    feat: Recoil & Auth State 추가

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RecoilState } from 'recoil'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RecoilState>
       <App />
+    </RecoilState>
   </React.StrictMode>
 )
---
+++ client/src/state/auth.jsx
@@ -0,0 +1,8 @@
+import {atom} from 'recoil';
+
+const authState = atom({
+  key: 'authState',
+  default: null
+})
+
+export default authState;
