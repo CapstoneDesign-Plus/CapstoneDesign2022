8136b270faa4373bcad464d3a5a60f6fc463abea
byungjin <byungjin.dev@gmail.com>
2022-11-12 22:58:08 +0900

    feat: Change NickName

--- server/src/api/v1/user/change.ts
+++ server/src/api/v1/user/change.ts
@@ -5,32 +5,29 @@ import { IUser } from "@/models/user";
 
 const router = Router();
 
-router.put('/name', ...validator.user_change_name, async (req, res)=> {
-  if (!req.user)
-    return res.redirect('/api/v1/user/login/web');
+router.put("/name", ...validator.user_change_name, async (req, res) => {
+  if (!req.user) return res.status(400).send("<h1>Login Please.</h1>");
 
-  await UserService
-    .getInstance()
-    .changeUsername(req.user as IUser, req.query['new_name'] as string);
+  await UserService.getInstance().changeUsername(
+    req.user as IUser,
+    req.query["new_name"] as string
+  );
 
   return res.sendStatus(200);
 });
 
+router.put("/password", ...validator.user_change_password, async (req, res) => {
+  if (!req.user) return res.redirect("/api/v1/user/login/web");
 
-router.put('/password', ...validator.user_change_password, async (req, res)=> {
-  
-  if (!req.user) return res.redirect('/api/v1/user/login/web');
+  const isSuccess = await UserService.getInstance().changePassword(
+    req.user as IUser,
+    req.body["old_password"],
+    req.body["new_password"]
+  );
 
-  const isSuccess = await UserService
-    .getInstance()
-    .changePassword(req.user as IUser,
-        req.body['old_password'],
-        req.body['new_password']);
-
-  if(isSuccess) return res.sendStatus(200);
+  if (isSuccess) return res.sendStatus(200);
 
   return res.sendStatus(400);
 });
-
 
 export default router;
