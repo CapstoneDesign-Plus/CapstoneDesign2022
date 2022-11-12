2e24a1c7855400815637196b971b2182c611d9ec
byungjin <byungjin.dev@gmail.com>
2022-11-12 23:21:13 +0900

    feat: Auth Check Return User

--- server/src/api/v1/user/auth.ts
+++ server/src/api/v1/user/auth.ts
@@ -5,107 +5,113 @@ import TokenService from "@/services/token";
 import UserService from "@/services/user";
 import { Router } from "express";
 
-
 const router = Router();
 
-router.get('/check', (req, res)=>{
-  return res.sendStatus(req.user ? 200 : 400);
-})
+router.get("/check", (req, res) => {
+  if (req.user) return res.send(req.user);
+  return res.sendStatus(400);
+});
 
-router.post('/logout', (req, res, next)=> {
-  req.logOut((err)=>{
-    if(err) return next(err);
-    return res.redirect('/');
+router.post("/logout", (req, res, next) => {
+  req.logOut((err) => {
+    if (err) return next(err);
+    return res.redirect("/");
   });
+});
 
-})
+router.post("/login", ...validator.user_login, (req, res, next) => {
+  passport.authenticate("local-login", (err, user, info) => {
+    if (err) return res.sendStatus(400);
 
-router.post('/login', ...validator.user_login, (req, res, next)=> {
-  passport.authenticate('local-login', (err, user, info)=>{
-    if(err) return res.sendStatus(400);
+    if (user) {
+      req.logIn(user as Express.User, async (err) => {
+        if (err) return next(err);
 
-    if(user) {
-      req.logIn(user as Express.User, async (err)=>{
-        if(err) return next(err);
-
-        const u = await UserService.getInstance().get(req.body['email']);
+        const u = await UserService.getInstance().get(req.body["email"]);
 
         return res.json(u);
-      })
-    }else{
+      });
+    } else {
       return res.sendStatus(400);
     }
-
   })(req, res, next);
 });
 
-router.get('/logout', (req, res) => {
+router.get("/logout", (req, res) => {
+  if (!req.user) return res.sendStatus(400);
 
-  if(!req.user) return res.sendStatus(400);
-
-  req.logout((err)=>{
-    req.session.destroy(()=>{});
+  req.logout((err) => {
+    req.session.destroy(() => {});
     res.sendStatus(200);
   });
-})
+});
 
-router.post('/signup', ...validator.user_signup, async (req, res)=> {
-  const isSuccess = await UserService
-    .getInstance()
-    .signup(req.body);
+router.post("/signup", ...validator.user_signup, async (req, res) => {
+  const isSuccess = await UserService.getInstance().signup(req.body);
 
-  if(isSuccess) return res.sendStatus(200);
+  if (isSuccess) return res.sendStatus(200);
 
   return res.sendStatus(400);
 });
 
-router.put('/password/:identifier', ...validator.user_put_reset_password,async (req, res) => {
-  const decodedId = decodeURIComponent(req.params.identifier);
+router.put(
+  "/password/:identifier",
+  ...validator.user_put_reset_password,
+  async (req, res) => {
+    const decodedId = decodeURIComponent(req.params.identifier);
 
-  const token = await TokenService.getInstance().get(decodedId);
+    const token = await TokenService.getInstance().get(decodedId);
 
-  if(token && TokenService.isValid(token)) {
-    const email = token.email;
+    if (token && TokenService.isValid(token)) {
+      const email = token.email;
 
-    const npassword = req.body['new_password'] as string;
+      const npassword = req.body["new_password"] as string;
 
-    await UserService.getInstance().setPassword(email, npassword);
+      await UserService.getInstance().setPassword(email, npassword);
 
-    return res.sendStatus(200);
+      return res.sendStatus(200);
+    }
+
+    return res.sendStatus(400);
   }
+);
 
-  return res.sendStatus(400);
-})
-
-router.get('/password/valid/:token', async(req, res)=> {
+router.get("/password/valid/:token", async (req, res) => {
   const decodedId = decodeURIComponent(req.params.token);
 
   const token = await TokenService.getInstance().get(decodedId);
 
-  if(token && TokenService.isValid(token)) {
+  if (token && TokenService.isValid(token)) {
     return res.sendStatus(200);
   }
 
   return res.sendStatus(400);
-})
-
-router.get('/password', ...validator.user_get_reset_password, async(req, res)=>{
-
-  const email = req.query.email as string;
-
-  const result = await TokenService
-    .getInstance()
-    .create(email, 10);
-
-  if(result) {
-    /**
-     * @TODO url 수정 필요 
-     */
-    const access_url = `http://bapsim.kro.kr/api/v1/auth/password/${encodeURIComponent(result)}`;
-
-    MailService.getInstance().sendMail(email, '비밀번호 재설정', `<div>${access_url}</div>`);
-  }
-
-  return res.sendStatus(result ? 200 : 400);
 });
+
+router.get(
+  "/password",
+  ...validator.user_get_reset_password,
+  async (req, res) => {
+    const email = req.query.email as string;
+
+    const result = await TokenService.getInstance().create(email, 10);
+
+    if (result) {
+      /**
+       * @TODO url 수정 필요
+       */
+      const access_url = `http://bapsim.kro.kr/api/v1/auth/password/${encodeURIComponent(
+        result
+      )}`;
+
+      MailService.getInstance().sendMail(
+        email,
+        "비밀번호 재설정",
+        `<div>${access_url}</div>`
+      );
+    }
+
+    return res.sendStatus(result ? 200 : 400);
+  }
+);
 export default router;
--- server/src/middleware/validator.ts
+++ server/src/middleware/validator.ts
@@ -1,124 +1,97 @@
-import { NextFunction, Request, Response } from 'express';
-import { body, param, query, validationResult } from 'express-validator';
+import { NextFunction, Request, Response } from "express";
+import { body, param, query, validationResult } from "express-validator";
 
-function ValidateErrorHandler(
-  req : Request, 
-  res : Response, 
-  next : NextFunction) {
-
+function ValidateErrorHandler(req: Request, res: Response, next: NextFunction) {
   const errors = validationResult(req);
-  if(!errors.isEmpty()){
-    return res.status(400).json({errors : errors.array()});
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
   }
   next();
 }
 
 const user_signup = [
-  body('username').exists(),
-  body('password').isLength({max: 20, min: 8}),
-  body('email').isEmail(),
-  ValidateErrorHandler
+  body("username").exists(),
+  body("password").isLength({ max: 20, min: 8 }),
+  body("email").isEmail(),
+  ValidateErrorHandler,
 ];
 
 const user_login = [
-  body('email').isEmail(),
-  body('password').exists(),
-  ValidateErrorHandler
+  body("email").isEmail(),
+  body("password").exists(),
+  ValidateErrorHandler,
 ];
 
-const user_get = [
-  param('email').isEmail(),
-  ValidateErrorHandler
-]
+const user_get = [param("email").isEmail(), ValidateErrorHandler];
 
 const notice_post = [
-  body('title').exists(),
-  body('content').exists(),
-  body('header').exists(),
-  ValidateErrorHandler
+  body("title").exists(),
+  body("content").exists(),
+  body("header").exists(),
+  ValidateErrorHandler,
 ];
 
 const notice_update = [
-  body('identifier').exists(),
-  body('title').exists(),
-  body('content').exists(),
-  body('header').exists(),
-  ValidateErrorHandler
-]
-
-const notice_delete = [
-  param('id').exists(),
-  ValidateErrorHandler
-]
-
-const notice_get = [
-  param('id').exists(),
-  ValidateErrorHandler
+  body("identifier").exists(),
+  body("title").exists(),
+  body("content").exists(),
+  body("header").exists(),
+  ValidateErrorHandler,
 ];
 
-const ticket_create = [
-  body('owner').exists(),
-  body('tclass').exists(),
-  ValidateErrorHandler
-]
+const notice_delete = [param("id").exists(), ValidateErrorHandler];
 
-const ticket_get = [
-  body('identifier').exists(),
-  ValidateErrorHandler
-]
+const notice_get = [param("id").exists(), ValidateErrorHandler];
+
+const ticket_create = [
+  body("owner").exists(),
+  body("tclass").exists(),
+  ValidateErrorHandler,
+];
+
+const ticket_get = [body("identifier").exists(), ValidateErrorHandler];
 
 const ticket_assign = [
-  body('identifier').exists(),
-  body('to').exists(),
-  ValidateErrorHandler
-]
+  body("identifier").exists(),
+  body("to").exists(),
+  ValidateErrorHandler,
+];
 
-const ticket_validate = [
-  body('identifier').exists(),
-  ValidateErrorHandler
-]
+const ticket_validate = [body("identifier").exists(), ValidateErrorHandler];
 
-const ticket_refund = [
-  body('identifier').exists(),
-  ValidateErrorHandler
-]
+const ticket_refund = [body("identifier").exists(), ValidateErrorHandler];
 
 const ticket_change_state = [
-  body('identifier').exists(),
-  body('state').exists(),
-  ValidateErrorHandler
-]
+  body("identifier").exists(),
+  body("state").exists(),
+  ValidateErrorHandler,
+];
 
 const user_point_give = [
-  query('email').exists(),
-  query('delta').isInt(),
+  query("email").exists(),
+  query("delta").isInt(),
   ValidateErrorHandler,
-]
+];
 
-const user_change_name = [
-  query('new_name').exists(),
-  ValidateErrorHandler
-]
+const user_change_name = [query("new_name").exists(), ValidateErrorHandler];
 
 const user_change_password = [
-  body('new_password').exists(),
-  ValidateErrorHandler
-]
+  body("old_password").exists(),
+  body("new_password").exists(),
+  ValidateErrorHandler,
+];
 
 const user_get_reset_password = [
-  query('email').isEmail(),
-  ValidateErrorHandler
-]
+  query("email").isEmail(),
+  ValidateErrorHandler,
+];
 
 const user_put_reset_password = [
-  body('new_password').exists(),
-  ValidateErrorHandler
-]
+  body("new_password").exists(),
+  ValidateErrorHandler,
+];
 
-const user_history = [
-  param('email').isEmail(),
-  ValidateErrorHandler
-]
+const user_history = [param("email").isEmail(), ValidateErrorHandler];
 
 export default {
   user_put_reset_password,
@@ -140,4 +113,4 @@ export default {
   ticket_validate,
   ticket_refund,
   ticket_change_state,
-}
+};
