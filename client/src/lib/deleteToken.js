fc33d19db5a455f953e9d5d6d09ace603f179a3f
byungjin <byungjin.dev@gmail.com>
2022-11-23 19:37:27 +0900

    fix: Fix Api path 'delete Token'

--- client/src/lib/deleteToken.js
+++ client/src/lib/deleteToken.js
@@ -8,7 +8,7 @@ import paxios from "./paxios";
 export default async function deleteToken(ids) {
   return new Promise((resolve, reject) => {
     paxios
-      .put("/v1/user/token", {
+      .put("/v1/user/delete/token", {
         ids,
       })
       .then((v) => {
