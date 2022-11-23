ad3c488e0cf16c16808550d0d5ab8e9ac9a39e1a
byungjin <byungjin.dev@gmail.com>
2022-11-23 16:55:45 +0900

    feat: Add Credential

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+axios.defaults.withCredentials = true;
+
 const instance = axios.create({
-  baseURL: 'http://bapsim.kro.kr/api/',
+  baseURL: "http://bapsim.kro.kr/api/",
   timeout: 5000,
-})
+  withCredentials: true,
+});
 
 export default instance;
