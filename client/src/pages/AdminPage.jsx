8f6d67916e472d582d2e9dc5b021678998514bdb
byungjin <byungjin.dev@gmail.com>
2022-11-24 14:58:59 +0900

    fix: Fix Jsconfig Base Url Path

--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -1,14 +1,25 @@
 import { Box } from "@mui/material";
-import { Navigate } from "react-router-dom";
-import { useRecoilValue } from "recoil";
+import { Route, Routes } from "react-router-dom";
 import Admin from "../components/Admin";
-import authState from "../state/auth";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import UserDashboard from "../components/Admin/UserDashboard";
+import LogDashboard from "../components/Admin/LogDashboard";
+import TicketDashboard from "../components/Admin/TicketDashboard";
+import TokenDashboard from "../components/Admin/TokenDashboard";
 
 const AdminPage = () => {
   return (
-    <Box width={750}>
-      <Admin></Admin>
-    </Box>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Box width={750}>
+        <Routes>
+          <Route path="user" element={<UserDashboard />} />
+          <Route path="log" element={<LogDashboard />} />
+          <Route path="ticket" element={<TicketDashboard />} />
+          <Route path="token" element={<TokenDashboard />} />
+        </Routes>
+      </Box>
+    </LocalizationProvider>
   );
 };
 
