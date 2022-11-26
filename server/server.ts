a5fb32b6632f9d04e19fe6ba3933fabdfb280869
byungjin <byungjin.dev@gmail.com>
2022-11-26 16:57:50 +0900

    feat: SSL

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,19 @@
+import App from "./src/app";
+import env from "./src/config";
+import https from "https";
+import fs from "fs";
 
-import App from './src/app';
-import env from './src/config';
-
-(async ()=> {
+(async () => {
   await App.connectForWork(); // For DB Connect or Something need time
   const app = App.buildExpressApp();
-  // 0.0.0.0은 ip를 ipv4로 읽기 위해
-  app.listen(env.PORT, '0.0.0.0', ()=>{
-    console.log("Server run!");
-  })
+
+  const option = {
+    key: fs.readFileSync(__dirname + "/cert/private.key"),
+    cert: fs.readFileSync(__dirname + "/cert/certificate.crt"),
+    ca: fs.readFileSync(__dirname + "/cert/ca_bundle.crt"),
+  };
+
+  https.createServer(option, app).listen(env.PORT, () => {
+    console.log("run!");
+  });
 })();
