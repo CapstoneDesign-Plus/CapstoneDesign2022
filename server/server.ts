40ed23949639615f92c475d25701caf621c37b7d
byungjin <byungjin.dev@gmail.com>
2022-11-26 17:16:39 +0900

    chore: SSL Return

--- server/server.ts
+++ server/server.ts
@@ -1,19 +1,11 @@
 import App from "./src/app";
 import env from "./src/config";
-import https from "https";
-import fs from "fs";
 
 (async () => {
   await App.connectForWork(); // For DB Connect or Something need time
   const app = App.buildExpressApp();
-
-  const option = {
-    key: fs.readFileSync(__dirname + "/cert/private.key"),
-    cert: fs.readFileSync(__dirname + "/cert/certificate.crt"),
-    ca: fs.readFileSync(__dirname + "/cert/ca_bundle.crt"),
-  };
-
-  https.createServer(option, app).listen(env.PORT, () => {
-    console.log("run!");
+  // 0.0.0.0은 ip를 ipv4로 읽기 위해
+  app.listen(env.PORT, "0.0.0.0", () => {
+    console.log("Server run!");
   });
 })();
