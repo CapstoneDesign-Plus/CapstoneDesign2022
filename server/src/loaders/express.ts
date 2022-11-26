f422248e30972fec8e00d42be8f93b1f5d04c641
byungjin <byungjin.dev@gmail.com>
2022-11-26 17:37:13 +0900

    feat: express session sameSite

--- server/src/loaders/express.ts
+++ server/src/loaders/express.ts
@@ -17,6 +17,8 @@ export default ({ app }: { app: express.Application }) => {
       saveUninitialized: true,
       cookie: {
         maxAge: 1000 * 60 * 5,
+        sameSite: "none",
+        secure: true,
       },
     })
   );
