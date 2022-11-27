d936420d9f2bb5b47c0dec8463ca99b85d7ff2fc
byungjin <byungjin.dev@gmail.com>
2022-11-27 21:52:12 +0900

    feat: Add QRscanner

--- client/src/components/QRRead.jsx
+++ client/src/components/QRRead.jsx
@@ -1,23 +1,54 @@
-import { Box } from "@mui/material";
-import { useEffect, useState } from "react";
-import { QrReader } from "react-qr-reader";
+import { Alert, Box, ToggleButton, ToggleButtonGroup } from "@mui/material";
+import { useRef, useState } from "react";
+import QrReader from "react-qr-scanner";
+import requestTicketStateUse from "../lib/requestTicketStateUse";
 
 const QRRead = () => {
-  const [data, setData] = useState("No Result");
+  const codeRef = useRef("");
 
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
+  const [type, setType] = useState("waiting");
+  const [passed, setPassed] = useState(false);
 
-  const onCatch = (result, error) => {
-    if (error) return;
+  const handleScan = (result) => {
+    if (result && result.text !== codeRef.current) {
+      codeRef.current = result.text;
+      requestTicketStateUse(type === "waiting" ? 0 : 1, result.text).then(
+        setPassed
+      );
+    }
+  };
 
-    setData(data);
+  const handleError = (error) => {
+    console.log(error);
   };
 
   return (
     <Box>
-      <QrReader />
+      <QrReader
+        style={{ width: "100%" }}
+        onScan={handleScan}
+        onError={handleError}
+        // facingMode="environment"
+        // style={{ width: "100%" }}
+      />
+
+      <Box style={{ display: "flex", justifyContent: "center" }}>
+        <ToggleButtonGroup
+          color="primary"
+          value={type}
+          exclusive
+          onChange={(e, v) => setType(v)}
+          aria-label="Platform"
+        >
+          <ToggleButton value="waiting">Waiting</ToggleButton>
+          <ToggleButton value="used">Used</ToggleButton>
+        </ToggleButtonGroup>
+      </Box>
+      {passed ? (
+        <Alert severity="success">올바른 식권</Alert>
+      ) : (
+        <Alert severity="error">잘못된 식권</Alert>
+      )}
     </Box>
   );
 };
---
+++ client/src/lib/requestTicketStateUse.js
@@ -0,0 +1,10 @@
+import paxios from "./paxios";
+
+// 0 - wait, 1 - use
+export default function requestTicketStateUse(type, value) {
+  return new Promise((resolve) => {
+    paxios.get(`/v1/ticket/use/${type}?id=${value}`).then((v) => {
+      resolve(v.data.ok);
+    });
+  });
+}
