0f6cc1e9c480a3448d0c8cb0eb3c77e62f70e090
byungjin <byungjin.dev@gmail.com>
2022-11-28 14:19:05 +0900

    feat: QRRead Change Logic

--- client/src/components/QRRead.jsx
+++ client/src/components/QRRead.jsx
@@ -4,23 +4,20 @@ const QrReader = lazy(() => import("react-web-qr-reader"));
 import requestTicketStateUse from "../lib/requestTicketStateUse";
 
 const QRRead = () => {
-  const codeRef = useRef();
-
-  if (!codeRef.current) codeRef.current = "";
+  const [code, setCode] = useState("");
+  const prevRef = useRef("");
 
   const [type, setType] = useState("waiting");
   const [passed, setPassed] = useState(false);
 
   const handleScan = (result) => {
-    if (result) {
-      console.log(result.data);
-    }
-
-    if (result && result.data !== codeRef.current) {
-      console.log("fetch");
-      codeRef.current = result.data;
+    if (result && result.data !== prevRef.current) {
+      prevRef.current = result.data;
       requestTicketStateUse(type === "waiting" ? 0 : 1, result.data).then(
-        setPassed
+        (v) => {
+          setCode(prevRef.current);
+          setPassed(v);
+        }
       );
     }
   };
@@ -32,14 +29,17 @@ const QRRead = () => {
   return (
     <Box>
       <QrReader
+        delay={1000}
         style={{ width: "100%" }}
         onScan={handleScan}
         onError={handleError}
-        // facingMode="environment"
+        facingMode="user"
         // style={{ width: "100%" }}
       />
 
-      <Box style={{ display: "flex", justifyContent: "center" }}>
+      <Box
+        style={{ display: "flex", justifyContent: "center", margin: "10px 0" }}
+      >
         <ToggleButtonGroup
           color="primary"
           value={type}
@@ -51,12 +51,13 @@ const QRRead = () => {
           <ToggleButton value="used">Used</ToggleButton>
         </ToggleButtonGroup>
       </Box>
-      {passed ? (
-        <Alert severity="success">올바른 식권</Alert>
-      ) : (
-        <Alert severity="error">잘못된 식권</Alert>
-      )}
-      <Box>token: {codeRef.current}</Box>
+      {code.length > 0 &&
+        (passed ? (
+          <Alert severity="success">올바른 식권</Alert>
+        ) : (
+          <Alert severity="error">잘못된 식권</Alert>
+        ))}
+      <Box style={{ fontSize: "12px" }}>{code}</Box>
     </Box>
   );
 };
