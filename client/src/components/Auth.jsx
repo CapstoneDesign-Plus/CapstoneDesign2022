8eab73f36dded7300dfc0566fb3dff5c50f5555e
byungjin <byungjin.dev@gmail.com>
2022-11-29 22:53:52 +0900

    feat: Add Auth Loading Centering

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import useAuthCheck from "../hook/useAuthCheck";
-import { CircularProgress } from "@mui/material";
+import { Box, CircularProgress } from "@mui/material";
 
 Auth.propTypes = {
   admin: PropTypes.bool,
@@ -11,7 +11,19 @@ Auth.propTypes = {
 function Auth({ el, admin = false }) {
   const { status, auth } = useAuthCheck();
 
-  if (status === "pending") return <CircularProgress />;
+  if (status === "pending")
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "50vh",
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    );
 
   return <>{auth && (!admin || auth.admin) ? el : <Navigate to="/" />}</>;
 }
