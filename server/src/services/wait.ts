1b871786e181984ac82f8faf0605eaa8e0a5a02a
byungjin <byungjin.dev@gmail.com>
2022-12-04 16:26:46 +0900

    fix: Wait Server CalcWaitTime

--- server/src/services/wait.ts
+++ server/src/services/wait.ts
@@ -1,5 +1,7 @@
+import env from "@/config";
 import { TicketClass } from "@/types/dto";
 import fs from "fs";
+import path from "path";
 import DietService, { DietDay } from "./diet";
 
 type WaitStorage = {
@@ -17,13 +19,16 @@ type CourseCount = {
 };
 
 interface WaitTimeTableRecord {
-    keyword: string,
-    supply: number,
-    ready: number,
-    unit: number,
+  keyword: string;
+  supply: number;
+  ready: number;
+  unit: number;
 }
 
-const RES_PATH: string = "../../../res/waittime_table.json";
+const RES_PATH: string = path.join(
+  env.ROOT_PATH,
+  "/resource/waittime_table.json"
+);
 
 export default class WaitService {
   private static instance: WaitService;
@@ -32,7 +37,9 @@ export default class WaitService {
   private storage: WaitStorage;
 
   private constructor() {
-    this.WAIT_TIME_TABLE = JSON.parse(fs.readFileSync(RES_PATH, {encoding: "utf-8"})) as WaitTimeTableRecord[];
+    this.WAIT_TIME_TABLE = JSON.parse(
+      fs.readFileSync(RES_PATH, { encoding: "utf-8" })
+    ) as WaitTimeTableRecord[];
     this.storage = {};
   }
 
@@ -65,9 +72,9 @@ export default class WaitService {
 
     for (const key in count) {
       const course = key as TicketClass;
-      const peopleCount = Object.values(this.storage)
-        .filter((v) => key === v.course)
-        .length;
+      const peopleCount = Object.values(this.storage).filter(
+        (v) => key === v.course
+      ).length;
 
       count[course] = {
         peopleCount,
@@ -78,13 +85,25 @@ export default class WaitService {
     return count;
   }
 
-  async CalcWaitSecond(course: TicketClass, peopleCount: number): Promise<number> {
-    let dayOfWeek: string | null = [null, "mon", "tue", "wed", "thr", "fri", null][new Date().getDay()]; // 가져올 날짜의 요일
-    if (!dayOfWeek)
-      return -1;  // error
+  async CalcWaitSecond(
+    course: TicketClass,
+    peopleCount: number
+  ): Promise<number> {
+    let dayOfWeek: string | null = [
+      null,
+      "mon",
+      "tue",
+      "wed",
+      "thr",
+      "fri",
+      null,
+    ][new Date().getDay()]; // 가져올 날짜의 요일
+    if (!dayOfWeek) return -1; // error
 
-    let diet: string[] = (await DietService.getInstance().getDiet())[dayOfWeek][course]
-    
+    let diet: string[] = (await DietService.getInstance().getDiet())[dayOfWeek][
+      course
+    ];
+
     let waitSecond: number = 0;
     this.WAIT_TIME_TABLE.forEach((record) => {
       if (record.keyword !== "기타") {
@@ -92,19 +111,22 @@ export default class WaitService {
           if (menu.includes(record.keyword)) {
             waitSecond += WaitService.addWaitTime(record, peopleCount);
           }
-        })
+        });
       }
     });
 
     if (waitSecond == 0) {
-      let record: WaitTimeTableRecord = this.WAIT_TIME_TABLE[0];  // 기타
+      let record: WaitTimeTableRecord = this.WAIT_TIME_TABLE[0]; // 기타
       waitSecond = WaitService.addWaitTime(record, peopleCount);
     }
     return waitSecond;
   }
 
   static addWaitTime(record: WaitTimeTableRecord, peopleCount: number): number {
-    return record.supply * peopleCount + record.ready * Math.ceil(peopleCount / record.unit);
+    return (
+      record.supply * peopleCount +
+      record.ready * Math.ceil(peopleCount / record.unit)
+    );
   }
 
   static getInstance() {
